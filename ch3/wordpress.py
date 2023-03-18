db49af1f427e4d49b825098c89ce2cad0d675bcd
Jun-Wei Lin <cs.castman@gmail.com>
2023-03-18 08:00:09 -0700

    Update wordpress.py

--- ch3/wordpress.py
+++ ch3/wordpress.py
@@ -5,23 +5,23 @@ from bs4 import BeautifulSoup
 def wordpress(url):
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text, "html.parser")
-    articles = []
-    for div in soup.find_all("article", "blog-post"):
-        article = {
-            "title": div.header.h1.a.text.strip(),
-            "date": div.header.find("div", "post-meta").time.span.text.strip(),
-            "link": div.h1.a["href"],
+    posts = []
+    for article in soup.find_all("article", "blog-post"):
+        post = {
+            "title": div.header.a.text.strip(),
+            "date": div.header.time.span.text.strip(),
+            "link": div.header.a["href"],
             "summary": div.find("div", "inner-desc").text.strip()
         }
-        articles.append(article)
-    return articles
+        posts.append(post)
+    return posts
 
 
 if __name__ == '__main__':
     URL = "https://decing.tw"
-    articles = wordpress(URL)
-    print(f"{len(articles)} articles retrieved. The first 3:")
+    posts = wordpress(URL)
+    print(f"{len(posts)} articles retrieved. The first 3:")
     print("--")
-    for article in articles[:3]:
+    for post in posts[:3]:
         print(article)
         print("--")
