36677eed2fd2927a269c2bb7504733939d65ee44
jwlin <cs.castman@gmail.com>
2023-03-27 15:17:18 -0700

    replace single quotes

--- ch3/ltn_news.py
+++ ch3/ltn_news.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 def ltn_news(url):
     articles = []
     resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    for li in soup.find('ul', 'list').find_all('li'):
-        a = li.find('a', 'tit')
+    soup = BeautifulSoup(resp.text, "html.parser")
+    for li in soup.find("ul", "list").find_all("li"):
+        a = li.find("a", "tit")
         article = {
             "link": a["href"],
             "title": a.div.h3.text.strip(),
@@ -17,8 +17,8 @@ def ltn_news(url):
     return articles
 
 
-if __name__ == '__main__':
-    print('自由時報今日熱門')
+if __name__ == "__main__":
+    print("自由時報今日熱門")
     URL = "https://news.ltn.com.tw/list/breakingnews/popular"
     ltn_articles = ltn_news(URL)
     for article in ltn_articles:
--- ch3/momo_search.py
+++ ch3/momo_search.py
@@ -8,21 +8,21 @@ def search_momo(url, query):
                              "AppleWebKit/537.36 (KHTML, like Gecko) "
                              "Chrome/109.0.0.0 Safari/537.36"}
     resp = requests.get(search_url, headers=headers)
-    soup = BeautifulSoup(resp.text, 'html.parser')
+    soup = BeautifulSoup(resp.text, "html.parser")
     items = []
     for li in soup.find_all("li", "goodsItemLi"):
         item = {
-            'name': li.find("h3", "prdName").text.strip(),
-            'price': li.find("b", "price").text.strip(),
-            'link': url + li.a['href']
+            "name": li.find("h3", "prdName").text.strip(),
+            "price": li.find("b", "price").text.strip(),
+            "link": url + li.a["href"]
         }
         items.append(item)
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     URL = "https://m.momoshop.com.tw/"
-    query = 'pixel 7 pro'
+    query = "pixel 7 pro"
     items = search_momo(URL, query)
     print(f"搜尋 {query} 共回傳 {len(items)} 筆資料")
     for i in items:
