9b0b6ca8f5759e6ee44add443cadd67388e3553b
jwlin <cs.castman@gmail.com>
2023-03-27 15:13:21 -0700

    replace single quotes

--- ch3/yahoo_movie.py
+++ ch3/yahoo_movie.py
@@ -7,35 +7,35 @@ from bs4 import BeautifulSoup
 def get_web_page(url):
     resp = requests.get(url)
     if resp.status_code != 200:
-        print('Invalid url:', resp.url)
+        print("Invalid url:", resp.url)
         return None
     else:
         return resp.text
 
 
 def get_movies(dom):
-    soup = BeautifulSoup(dom, 'html.parser')
+    soup = BeautifulSoup(dom, "html.parser")
     movies = []
-    divs = soup.find_all('div', 'release_info_text')
+    divs = soup.find_all("div", "release_info_text")
     for div in divs:
         movie = dict()
-        movie['expectation'] = div.find('div', 'leveltext').span.text.strip()
-        name_div = div.find('div', 'release_movie_name')
-        movie['ch_name'] = name_div.a.text.strip()
-        movie['eng_name'] = name_div.find('div', 'en').a.text.strip()
-        movie['release_date'] = get_date(div.find('div', 'release_movie_time').text)
-        movie['intro'] = div.find('div', 'release_text').text.strip()
-        foto_div = div.parent.find_previous_sibling('div', 'release_foto')
-        movie['poster_url'] = foto_div.a.img['data-src']
-        trailer_a = div.find_next_sibling('div', 'release_btn color_btnbox').find_all('a')[1]
-        movie['trailer_url'] = trailer_a['href'] if 'href' in trailer_a.attrs else ''
+        movie["expectation"] = div.find("div", "leveltext").span.text.strip()
+        name_div = div.find("div", "release_movie_name")
+        movie["ch_name"] = name_div.a.text.strip()
+        movie["eng_name"] = name_div.find("div", "en").a.text.strip()
+        movie["release_date"] = get_date(div.find("div", "release_movie_time").text)
+        movie["intro"] = div.find("div", "release_text").text.strip()
+        foto_div = div.parent.find_previous_sibling("div", "release_foto")
+        movie["poster_url"] = foto_div.a.img["data-src"]
+        trailer_a = div.find_next_sibling("div", "release_btn color_btnbox").find_all("a")[1]
+        movie["trailer_url"] = trailer_a["href"] if "href" in trailer_a.attrs else ""
         movies.append(movie)
     return movies
 
 
 def get_date(date_str):
     # e.g. "上映日期：2023-02-12" -> match.group(0): "2023-02-12"
-    pattern = '\d+-\d+-\d+'
+    pattern = "\d+-\d+-\d+"
     match = re.search(pattern, date_str)
     if match is None:
         return date_str
@@ -43,13 +43,13 @@ def get_date(date_str):
         return match.group(0)
 
 
-if __name__ == '__main__':
-    URL = 'https://tw.movies.yahoo.com/movie_thisweek.html'
+if __name__ == "__main__":
+    URL = "https://tw.movies.yahoo.com/movie_thisweek.html"
     page = get_web_page(URL)
     if page:
         movies = get_movies(page)
         print(f"{len(movies)} movies retrieved. Titles:")
         for movie in movies:
             print(movie["ch_name"])
-        with open('yahoo_movies.json', 'w', encoding='utf-8') as f:
+        with open("yahoo_movies.json", "w", encoding="utf-8") as f:
             json.dump(movies, f, indent=2, sort_keys=True, ensure_ascii=False)
